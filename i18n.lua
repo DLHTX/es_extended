b3ac7a315464accfe795ab8748275d3347d82ac6
MaLaGeBe <lpj.vip@foxmail.com>
2019-11-29 18:45:51 +0800

    Update i18n.lua

--- i18n.lua
+++ i18n.lua
@@ -1,7 +1,9 @@
-if GetCurrentLanguageId() == 9 then
-	Config.Locale = 'tc'
-elseif GetCurrentLanguageId() == 12 then
-	Config.Locale = 'sc'
-else 
-	Config.Locale = 'en'
-end
+Citizen.CreateThread(function()
+	if GetCurrentLanguageId() == 9 then
+		Config.Locale = "tc"
+	elseif GetCurrentLanguageId() == 12 then
+		Config.Locale = "sc"
+	else
+		Config.Locale = "en"
+	end
+end)
