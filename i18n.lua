818badb33e25ff99c9e17edeb6a55497f4220b7c
麻辣戈壁 <lpj.vip@foxmail.com>
2020-03-06 15:48:58 +0800

    Update i18n.lua

--- i18n.lua
+++ i18n.lua
@@ -1,9 +1,7 @@
-Citizen.CreateThread(function()
-	if GetCurrentLanguageId() == 9 then
-		Config.Locale = "tc"
-	elseif GetCurrentLanguageId() == 12 then
-		Config.Locale = "sc"
-	else
-		Config.Locale = "en"
-	end
-end)
+if GetCurrentLanguageId() == 9 then
+	Config.Locale = "tc"
+elseif GetCurrentLanguageId() == 12 then
+	Config.Locale = "sc"
+else
+	Config.Locale = "en"
+end
